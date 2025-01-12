191f6b021cbbfa9a63b0469d8e44de04ea881261
Chee Kian Teoh <teoh4770@mylaurier.ca>
2025-01-12 14:10:53 -0500

    refactor: Update Confetti component to increase the number of pieces

--- src/common/components/Confetti/Confetti.tsx
+++ src/common/components/Confetti/Confetti.tsx
@@ -4,7 +4,14 @@ import ConfettiLib from "react-confetti";
 const Confetti = () => {
   const { width, height } = useWindowSize();
 
-  return <ConfettiLib width={width} height={height} recycle={false} />;
+  return (
+    <ConfettiLib
+      width={width}
+      height={height}
+      numberOfPieces={1000}
+      recycle={false}
+    />
+  );
 };
 
 export { Confetti };
