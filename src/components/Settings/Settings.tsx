9024c7107e5a10860b6014522a013c147f437dbf
Chee Kian Teoh <teoh4770@mylaurier.ca>
2025-03-02 23:39:10 -0500

    refactor: move form handlers logic outside of markup

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -14,10 +14,26 @@ const Settings = forwardRef<Ref, SettingProps>((_, ref) => {
   const setting = useSettingContext();
   const [isFormValid, setIsFormValid] = useState(true);
 
+  // Trigger validation on form change
   const handleFormChange = (e: React.FormEvent) => {
     const form = e.currentTarget as HTMLFormElement;
+    console.log("is form valid?: ", form.checkValidity())
     setIsFormValid(form.checkValidity()); // Updates based on built-in HTML5 validation
   };
+
+  const handleFormSubmit = (e: React.FormEvent) => {
+    // Handle form value here
+    const form = e.currentTarget as HTMLFormElement;
+    const settingFormData = new FormData(form);
+
+    // Update timer setting
+    setting.timerSettingHandler(
+      Number(settingFormData.get("pomodoro-duration")!),
+      Number(settingFormData.get("break-duration")!),
+      Boolean(settingFormData.get("auto-start-pomodoro")!),
+      Boolean(settingFormData.get("auto-start-break")!),
+    );
+  }
 
   return (
     <dialog
@@ -27,19 +43,8 @@ const Settings = forwardRef<Ref, SettingProps>((_, ref) => {
     >
       <form
         method="dialog"
-        onChange={handleFormChange} // Trigger validation on change
-        onSubmit={(e) => {
-          // Handle form value here
-          const settingFormData = new FormData(e.currentTarget);
-
-          // Update timer setting
-          setting.timerSettingHandler(
-            Number(settingFormData.get("pomodoro-duration")!),
-            Number(settingFormData.get("break-duration")!),
-            Boolean(settingFormData.get("auto-start-pomodoro")!),
-            Boolean(settingFormData.get("auto-start-break")!),
-          );
-        }}
+        onChange={handleFormChange} 
+        onSubmit={handleFormSubmit}
       >
         {/* SettingsHeader gets the same ref so it can use to close the model */}
         <SettingsHeader dialogRef={ref as React.RefObject<HTMLDialogElement>} />
